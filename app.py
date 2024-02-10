7b9425c3fbd2cdd69e77096cb73506e1640f6cfb
MihirSethi <misethi@cisco.com>
2024-02-10 07:00:29 +0530

    change in / route added index3.html to it

--- app.py
+++ app.py
@@ -2,15 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__,template_folder='templates', static_folder='static')
  
-@app.route("/")
-def welcome():
-    return render_template('starter.html')
+# @app.route("/")
+# def welcome():
+#     return render_template('starter.html')
  
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/index3')
+@app.route('/]')
 def btsp():
     return render_template('index3.html')
  
